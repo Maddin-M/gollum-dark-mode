010721cb5450685f0aad488c7850cce19bcd23d1
maddin <martin.manka@gmx.de>
2026-01-22 09:32:58 +0100

    Apply ace editor style conditionally

--- custom.js
+++ custom.js
@@ -1,6 +1,8 @@
 // see: https://github.com/gollum/gollum/issues/1656#issuecomment-785096494
 $(document).ready(function () {
-  window.ace.config.setModuleUrl("ace/theme/twilight", "http://ajaxorg.github.io/ace-builds/src-noconflict/theme-twilight.js")
-  window.ace_editor.setTheme("ace/theme/twilight")
-  window.ace_editor.setOption("showPrintMargin", false)
+  if (window.ace && window.ace_editor) {
+    window.ace.config.setModuleUrl("ace/theme/twilight", "http://ajaxorg.github.io/ace-builds/src-noconflict/theme-twilight.js")
+    window.ace_editor.setTheme("ace/theme/twilight")
+    window.ace_editor.setOption("showPrintMargin", false)
+  }
 })
