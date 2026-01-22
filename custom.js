4741a423d38c45fc3732ddcde3d443da3249678c
maddin <martin.manka@gmx.de>
2026-01-22 13:15:23 +0100

    Add support for Mermaid dark mode

--- custom.js
+++ custom.js
@@ -1,8 +1,13 @@
-// see: https://github.com/gollum/gollum/issues/1656#issuecomment-785096494
-$(document).ready(function () {
+$(document).ready(() => {
+  // ace editor dark mode
+  // see: https://github.com/gollum/gollum/issues/1656#issuecomment-785096494
   if (window.ace && window.ace_editor) {
     window.ace.config.setModuleUrl("ace/theme/twilight", "http://ajaxorg.github.io/ace-builds/src-noconflict/theme-twilight.js")
     window.ace_editor.setTheme("ace/theme/twilight")
     window.ace_editor.setOption("showPrintMargin", false)
+  }
+  // mermaid dark mode
+  if (mermaid) {
+    mermaid.initialize({ theme: "dark" })
   }
 })
